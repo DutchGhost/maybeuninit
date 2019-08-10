248d5ebf90ec3b55876b69f89204e028ab679630
Dodo <kasper199914@gmail.com>
2019-08-10 20:43:46 +0200

    repo setup

---
+++ src/main.zig
@@ -0,0 +1,98 @@
+/// A wrapper type to construct uninitialized instances of `T`.
+pub inline fn MaybeUninit(comptime T: type) type {
+    return packed union {
+        value: T,
+        uninit: []align(2048) void,
+
+        const Self = @This();
+
+        /// Creates a new initialized `MaybeUninit(T)` initialized with the given value.
+        inline fn init(value: T) Self {
+            return Self{ .value = value };
+        }
+
+        /// Creates a new `MaybeUninit(T)` in an uninitialized state.
+        inline fn uninit() Self {
+            return Self{ .uninit = &([1]void{{}} ** 1024) };
+        }
+
+        /// Creates a new `MaybeUnint(T)` in an uninitialized state, with the memory being filled with `0` bytes.
+        /// It depends on `T` whether that already makes for proper initialization.
+        inline fn zeroed() Self {
+            var u = Self.uninit();
+
+            var bytes = @ptrCast([*]u8, u.as_mut_ptr());
+            @memset(bytes, 0, @sizeOf(T));
+
+            return u;
+        }
+
+        /// Gets a pointer to the contained value.
+        inline fn as_ptr(self: *const Self) *const T {
+            return &self.value;
+        }
+
+        /// Gets a mutable pointer to the contained value
+        inline fn as_mut_ptr(self: *Self) *T {
+            return &self.value;
+        }
+
+        /// Extracts the value from the `MaybeUninit(T)` container.
+        inline fn assume_init(self: Self) T {
+            return self.value;
+        }
+
+        /// Reads the value from the `MabeUninit(T)` container.
+        /// Whenever possible, prefer to use [`MaybeUninit::assume_init`] instead, which prevents duplicating the content of the `MaybeUninit(T)`.
+        inline fn read(self: *const Self) T {
+            return self.as_ptr().*;
+        }
+
+        /// Sets the value of the `MaybeUninit(T)`.
+        inline fn write(self: *Self, value: T) void {
+            self.value = value;
+        }
+
+        /// Gets a pointer to the first element of the slice.
+        inline fn first_ptr(this: []const Self) *const T {
+            return @ptrCast(*const T, this.ptr);
+        }
+
+        /// Gets a mutable pointer to the first element of the slice.
+        inline fn first_ptr_mut(this: []Self) *Self {
+            return @ptrCast(*T, this.ptr);
+        }
+    };
+}
+
+const std = @import("std");
+const testing = std.testing;
+
+test "test usage" {
+    var maybe = MaybeUninit(u64).zeroed();
+
+    testing.expectEqual(maybe.read(), 0);
+
+    maybe.write(10);
+    testing.expectEqual(maybe.read(), 10);
+}
+
+test "first_ptr" {
+    var maybe = [2]MaybeUninit(i64){ MaybeUninit(i64).init(10), MaybeUninit(i64).uninit() };
+
+    var ptr = MaybeUninit(i64).first_ptr(&maybe);
+
+    testing.expectEqual(ptr.*, 10);
+}
+
+test "assert size" {
+    testing.expectEqual(@sizeOf(MaybeUninit(u64)), @sizeOf(u64));
+    testing.expectEqual(@sizeOf(MaybeUninit(?*u64)), @sizeOf(*u64));
+    testing.expectEqual(@sizeOf(?MaybeUninit(*u64)), 16);
+}
+
+test "assert align" {
+    testing.expectEqual(@alignOf(MaybeUninit(u64)), @alignOf(u64));
+    testing.expectEqual(@alignOf(MaybeUninit(?*u64)), @alignOf(*u64));
+    testing.expectEqual(@alignOf(?MaybeUninit(*u64)), 8);
+}
